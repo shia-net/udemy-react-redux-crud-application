922be5f9f549d8184d1bdd367c58304858d03e3f
shia-net <shi.net.pc@gmail.com>
2020-11-23 18:17:31 +0900

    :+1: why-jsx

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -2,8 +2,19 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <div><h1>Hello World</h1></div>;
+    return <h1>Hello World</h1>;
   }
 }
+
+// Javascriptで書いた場合
+// class App extends Component {
+//   render() {
+//     return React.createElement(
+//       "div",
+//       null,
+//       "Hello, world!!",
+//     );
+//   }
+// }
 
 export default App;
