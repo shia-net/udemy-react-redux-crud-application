4eb6667c5a7f490dc5bd8292f443325fa0b9dc9c
shia-net <shi.net.pc@gmail.com>
2020-12-03 19:01:07 +0900

    :+1: learn jsx syntax

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -3,7 +3,17 @@ import React, { Component } from 'react';
 // JSXで書いた場合
 class App extends Component {
   render() {
-    return <h1>Hello World</h1>;
+//    const greeting = "Hi, Tom!";
+//    const dom = <h1 className="foo">{greeting}</h1>;
+//    return dom;
+//    return <input type="text" onClick={() => {console.log("I am clicked.")}}/>;
+//    return <input type="text" onChange={() => {console.log("I am clicked.")}}/>;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked!")}} />
+      </React.Fragment>
+    );
   }
 }
 
