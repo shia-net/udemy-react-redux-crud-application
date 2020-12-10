af6b0e6bdd340a52b4b4df7b45ee95d0d2676ca2
shia-net <shi.net.pc@gmail.com>
2020-12-10 13:11:40 +0900

    :+1: create class component and functional component

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 // JSXで書いた場合
-class App extends Component {
-  render() {
-//    const greeting = "Hi, Tom!";
-//    const dom = <h1 className="foo">{greeting}</h1>;
-//    return dom;
-//    return <input type="text" onClick={() => {console.log("I am clicked.")}}/>;
-//    return <input type="text" onChange={() => {console.log("I am clicked.")}}/>;
-    return (
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onChange={() => {console.log("I am clicked!")}} />
-      </React.Fragment>
-    );
-  }
-}
-
-// Javascriptで書いた場合
 // class App extends Component {
 //   render() {
-//     return React.createElement(
-//       "div",
-//       null,
-//       "Hello, world!!",
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked!")}} />
+//       </React.Fragment>
 //     );
 //   }
 // }
+
+const App = () => {
+  return (
+  <div>
+    <Cat />
+    <Cat />
+    <Cat />
+    <Cat />
+  </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
+}
 
 export default App;
